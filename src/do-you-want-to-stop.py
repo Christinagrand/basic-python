9993dce60b05967158e3af0d78ed9787aed782b3
Christinagrand <christinagrand@hotmail.com>
2023-09-08 09:42:30 +0200

    Gorms

--- src/do-you-want-to-stop.py
+++ src/do-you-want-to-stop.py
@@ -1,12 +1,4 @@
 # You should write a loop around this question, and keep asking until
 # the answer is "yes" (lower case).
-n = 10
-i = 1
-while i <= n:
-  print("Hello, World!")
-  stop = input("Do you want to stop?")
-  if stop == "yes":
-    print(f"Stopped after {i}/{n} iterations.")
-    break
-  i += 1
-  print(f"All {n}/{n} iterations finished!")
+while input("Do you want to stop?") != "yes":
+    None
